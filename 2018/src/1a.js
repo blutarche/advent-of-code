de797992fe012ed6fbfafb91ab79f0a08745271a
Aikdanai Sidhikosol <aikdanai.s@cleverse.com>
2018-12-01 22:32:44 +0700

    completed 1a

--- 2018/src/1a.js
+++ 2018/src/1a.js
@@ -1,9 +1,27 @@
-import { readInput, writeAnswer } from './utils'
+import _ from 'lodash'
+
+import { readInput, writeAnswer, addAnswerLine, getAnswer } from './utils'
 
 const main = async () => {
   const input = await readInput('1a')
-  console.log(input)
-  await writeAnswer(['a', 'b'], '1a')
+
+  let sum = 0
+  const len = input.length
+
+  for (let i = 0; i < len; i++) {
+    const line = input[i]
+    const operator = line[0]
+    const number = _.toNumber(line.slice(1))
+    console.log(operator, number, typeof number)
+    if (operator === '+') {
+      sum += number
+    } else if (operator === '-') {
+      sum -= number
+    }
+    console.log('sum:', sum)
+  }
+  addAnswerLine(sum)
+  await writeAnswer(getAnswer(), '1a')
 }
 
 main()
--- 2018/src/utils.js
+++ 2018/src/utils.js
@@ -1,22 +1,28 @@
 import { promises as fs } from 'fs'
 import path from 'path'
+import _ from 'lodash'
 
-export const writeAnswer = (arrayOfLines, filename) => {
+export const writeAnswer = (answer, filename) => {
+  console.log('******************')
+  console.log('*     ANSWER     *')
+  console.log('******************')
+  console.log(answer)
   return new Promise((resolve, reject) => {
-    fs.writeFile(
-      path.join(__dirname, '../answer/', filename),
-      arrayOfLines.join('\n'),
-      err => {
-        if (err) {
-          reject(err)
-        }
-        resolve()
+    fs.writeFile(path.join(__dirname, '../answer/', filename), answer, err => {
+      if (err) {
+        reject(err)
       }
-    )
+      resolve()
+    })
   })
 }
 
 export const readInput = async fileName => {
   const content = await fs.readFile(path.join(__dirname, '../input/', fileName))
-  return content.toString().split('\n')
+  const splitted = content.toString().split('\n')
+  return _.filter(splitted, s => s && s.length > 0)
 }
+
+const answerLines = []
+export const addAnswerLine = str => answerLines.push(str)
+export const getAnswer = () => answerLines.join('\n')
